099d1f5af506cc5fddf78578cf005f72c3468124
Astra97 <abelestrada5@yahoo.com>
2020-09-23 18:38:14 -0700

    used fs to generate html webpage

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+
 //var commandLineArgs = process.argv;
 //console.log(commandLineArgs);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+ const profileDataArgs = process.argv.slice(2, process.argv.length);
+ const [name, github] = profileDataArgs;
+
+// console.log(profileDataArgs);
 
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i+=1){
-    console.log(profileDataArr[i]);
-    }
-};
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i+=1){
+//     console.log(profileDataArr[i]);
+//     }
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
 
-console.log('================');
+// console.log('================');
 
-profileDataArgs.forEach(profileItem => console.log(profileItem));
+// profileDataArgs.forEach(profileItem => console.log(profileItem));
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+  
